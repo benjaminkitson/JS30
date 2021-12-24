2a357c357ef596c41e797a8362db0a6b0b4b4f62
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-24 23:52:33 +0000

    added slider functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -20,6 +20,10 @@ function skip() {
   video.currentTime += skip
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
@@ -27,3 +31,5 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => {
   button.addEventListener('click', skip)
 });
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
