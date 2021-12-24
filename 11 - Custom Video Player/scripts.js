7c2288d0b8eb09397b27a8511f3e52f63353e623
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-24 12:43:05 +0000

    added eventlistener and play/pause funtion to video player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,5 +3,11 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip');
-const ranges = player.querySelectorAll('.player__slider')
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+  video.paused ? video.play() : video.pause()
+}
+
+video.addEventListener('click', togglePlay)
