ff975acc42fd7c625e4eafb9be60d6d2ac7e0a36
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-07 21:04:34 +0000

    added filters - greenScreen() doesn't work, both in demo script and written one

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -15,6 +15,51 @@ function getVideo() {
     });
 }
 
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] += 50;
+    pixels.data[i + 1] -= 25;
+    pixels.data[i + 2] -= 105;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i]
+    pixels.data[i + 300] = pixels.data[i + 1]
+    pixels.data[i - 300] = pixels.data[i + 2]
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
 function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -24,6 +69,10 @@ function paintToCanvas() {
 
   setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // pixels = redEffect(pixels);
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16)
 }
 
@@ -38,6 +87,7 @@ function takePhoto() {
   link.innerHTML = `<img src="${data}" alt="Silly nonsense"/>`;
   strip.insertBefore(link, strip.firstChild);
 }
+
 
 getVideo()
 
